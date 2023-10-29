a6e4ca79925450b94eb907e74687d437c3242a8b
AmitKumar <rajputamit0338@gmail.com>
2023-10-30 02:03:53 +0530

    change

--- src/Pages/ExerciseDetail.js
+++ src/Pages/ExerciseDetail.js
@@ -34,6 +34,7 @@ const ExerciseDetail = () => {
     <Box sx={{ mt: { lg: '96px', xs: '60px' } }}>
       <Detail exerciseDetail={exerciseDetail} />
       <ExerciseVideos exerciseVideos={exerciseVideos} name={exerciseDetail.name} />
+      
     </Box>
   );
 };
